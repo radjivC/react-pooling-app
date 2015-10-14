e949f4f0c5641c3ae6e7cd328e777415cf8f6e86
radjivC <carrereradjiv@yahoo.fr>
2015-10-14 23:23:26 +0200

    add socket.io support

--- app-server.js
+++ app-server.js
@@ -4,6 +4,12 @@ var app = express();
 
 app.use(express.static('./public'));
 app.use(express.static('./node_modules/bootstrap/dist'));
-app.listen(3000);
 
-console.log("Polling server is runing at 'http://localhost:3000'")
+var server = app.listen(3000);
+var io =require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket){
+  console.log("connected: %s", socket.id);
+});
+
+console.log("Polling server is runing at 'http://localhost:3000'");
