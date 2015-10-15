12d341d55d95c886aa3a15a4c75552141457bb56
radjivC <carrereradjiv@yahoo.fr>
2015-10-15 14:02:51 +0200

    add presentation title

--- app-server.js
+++ app-server.js
@@ -1,15 +1,28 @@
 var express = require('express')
-
 var app = express();
+
+var connections = [];
+var title = "Untitled Presentation";
 
 app.use(express.static('./public'));
 app.use(express.static('./node_modules/bootstrap/dist'));
 
 var server = app.listen(3000);
-var io =require('socket.io').listen(server);
+var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function(socket){
-  console.log("connected: %s", socket.id);
+
+  socket.once('disconnect', function(){
+    connections.splice(connections.indexOf(socket), 1);
+    socket.disconnect();
+    console.log("Disconnect socket: %s socket remaining.", connections.length)
+  });
+  socket.emit('welcome', {
+    title: title
+  });
+
+  connections.push(socket);
+  console.log("connected: %s sockets", connections.length);
 });
 
 console.log("Polling server is runing at 'http://localhost:3000'");
--- components/app.js
+++ components/app.js
@@ -1,16 +1,41 @@
 var React = require('react');
 var io = require('socket.io-client');
+var Header = require('./parts/Header');
 
 var APP = React.createClass({
+
+  getInitialState(){
+    return{
+        status:'disconnected',
+        title: ''
+    }
+  },
+
   componentWillMount(){
     this.socket = io('http://localhost:3000');
-    this.socket.on('connect', this.connect)
+    this.socket.on('connect', this.connect);
+    this.socket.on('disconnect', this.disconnect);
+    this.socket.on('welcome', this.welcome);
   },
+
   connect() {
-    alert("Connected:"+ this.socket.id)
+    this.setState({status:'connected'});
   },
+
+  disconnect(){
+    this.setState({status:'disconnected'});
+  },
+
+  welcome(serverState){
+    this.setState({title: serverState.title});
+  },
+
 	render() {
-		return (<h1>Hello World form React</h1>);
+		return (
+      <div>
+        <Header title = {this.state.title} status= {this.state.status}/>
+      </div>
+    );
 	}
 });
 
